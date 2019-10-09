f0276d03d14eadb92df6e955bd77d049daa983ce
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-09 09:59:24 -0600

    Add rooms_by_category test for house class

--- test/house_test.rb
+++ test/house_test.rb
@@ -30,7 +30,22 @@ class HouseTest < Minitest::Test
     room_2 = Room.new(:bedroom, 11, 15)
     @house.add_room(room_1)
     @house.add_room(room_2)
-    
+
     assert_equal [room_1, room_2], @house.rooms
+  end
+
+  def test_rooms_can_be_called_by_category
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    @house.add_room(room_1)
+    @house.add_room(room_2)
+    @house.add_room(room_3)
+    @house.add_room(room_4)
+    require "pry"; binding.pry
+    assert_equal [room_1, room_2], @house.rooms_from_category(:bedroom)
+    assert_equal [room_3], @house.rooms_from_category(:living_room)
+    assert_equal [], @house.rooms_from_category(:kitchen)
   end
 end
