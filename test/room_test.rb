41f9a5d352e1b33b516aa44dce1f29bd5fb2abf2
Dylan Connolly <dylanconnolly145@gmail.com>
2019-10-09 09:14:29 -0600

    Add additional assertion for category and area tests

--- test/room_test.rb
+++ test/room_test.rb
@@ -14,9 +14,17 @@ class RoomTest < Minitest::Test
 
   def test_it_has_a_category
     assert_equal :bedroom, @room.category
+
+    room_2 = Room.new(:kitchen, 10, 15)
+
+    assert_equal :kitchen, room_2.category
   end
 
   def test_area_can_be_calculated
     assert_equal 130, @room.area
+
+    room_2 = Room.new(:kitchen, 10, 15)
+
+    assert_equal 150, room_2.area
   end
 end
